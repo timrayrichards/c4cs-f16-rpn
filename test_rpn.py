227cd5d2f707328f10b63c1c419cab315e7fe052
Tim Richards <timricha@umich.edu>
2016-10-15 14:29:51 -0400

    added ^ operator to test suite

--- test_rpn.py
+++ test_rpn.py
@@ -20,3 +20,6 @@ class TestBasics(unittest.TestCase):
 	def test_badstring(self):
 		with self.assertRaises(TypeError):
 			rpn.calculate("1 2 3 +")
+        def test_carat(self):
+                result = rpn.calculate("2 3 ^")
+                self.assertEqual(8, result)
